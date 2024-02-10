744545ff5cc07c145da7ef9f174ade6d70870bf8
SriniPNV <pnvsrinivas@outlook.com>
2024-02-10 11:57:19 +0530

    Implementation: Added time

--- api/function_app.py
+++ api/function_app.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 import logging
 import json
+from datetime import datetime
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -26,5 +27,26 @@ def message(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse(
              json.dumps({ "message": "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."}),
+             status_code=200
+        )
+
+@app.route(route="htptriger", auth_level=func.AuthLevel.ANONYMOUS)
+def htptriger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             f"{datetime.now()} => This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
